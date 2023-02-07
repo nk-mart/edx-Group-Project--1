5cf125d1eeb264aee3895ea25deab295e4036ffc
nk-mart <113395569+nk-mart@users.noreply.github.com>
2023-02-07 02:10:40 +0000

    POC
    POC
    
    HTML search bar
    JS code

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,28 @@
-alert("wow this is a test")
+
+
+
+// Proof of Concept 
+
+// key
+var api_key ='8143e65c-7c54-4b44-9179-9454f8ba3d2e'
+
+// Search Parameter for flight
+var flight_iata ='AA6'
+
+// Flight API Fetch URL
+var Flight_Api_URL = `https://airlabs.co/api/v9/flight?flight_iata=${flight_iata}&api_key=${api_key}`
+
+
+
+function FlightDataSearch(){
+fetch("sampledata.txt")
+
+.then(response =>response.json())
+.then(flightData => {
+    console.log(flightData)
+})
+
+}
+
+
+FlightDataSearch()
