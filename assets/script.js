252592975ca149230ae2d5bfc190c757ef4353d2
nk-mart <113395569+nk-mart@users.noreply.github.com>
2023-02-14 01:42:59 +0000

    Update JS .
    Event Listener added to search btn, takes value from text to complete api call.

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@
 const api_key ='8143e65c-7c54-4b44-9179-9454f8ba3d2e'
 
 // Search Parameter for flight
-let flight_iata ='AA6'
+let flight_iata ='AI126'
 
 // Flight API Fetch URL
 let Flight_Api_URL = `https://airlabs.co/api/v9/flight?flight_iata=${flight_iata}&api_key=${api_key}`
@@ -35,32 +35,32 @@ while(radmonNum.length < 3){
 
 // Flight API Search 
 function FlightDataSearch(){
-fetch("sampledata.txt")
+    
+let fligthNumber = flightSearchText.value.replaceAll(" ", "")
 
-.then(response =>response.json())
-.then(flightData => {
-    //console.log(flightData)
+fetch(`https://airlabs.co/api/v9/flight?flight_iata=${fligthNumber}&api_key=8143e65c-7c54-4b44-9179-9454f8ba3d2e`)
+    .then(response => response.json())
+    .then(flightData => {
+    console.log(flightData)
 
 
     //Parse Flight Info Data
-
-let FlightNumber = flightData.request.params.flight_iata
-let departTime = flightData.response.deptime
-let arrivalTime = flightData.response.arr_time
-let flightTime = flightData.response.duration
+        let FlightNumber = flightData.request.params.flight_iata
+        let departTime = flightData.response.dep_time
+        let arrivalTime = flightData.response.arr_time
+        let flightTime = flightData.response.duration
 
 
 //console.log(flightTime)
-
-let card = document.querySelector('.card-body')
+        let card = document.querySelector('.card-body')
 
 // togle card info 
-cardcontainer.classList.toggle("d-none")
+    //cardcontainer.classList.toggle("d-none")
 
 
 //console.log(cardcontainer.classList)
 
-card.innerHTML = `
+        card.innerHTML = `
 <p>Flight Number: ${FlightNumber}</p>
 <p> Flight Depart Time: ${departTime}</p>
 <p>Flight Arrival Time: ${arrivalTime}</p>
@@ -73,70 +73,68 @@ card.innerHTML = `
 
 
 
- flightbtn.addEventListener("click",function(){
-        console.log(flightSearchText.value.replaceAll(" ",""))
- } )
+ flightbtn.addEventListener("click",FlightDataSearch)
 
 
 
-
-
-
-
-// API CALL to FETCH TRENDING  Movie of they day
-fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=c1d91b870e49691263e4f7d72633542c")
-
- .then(response =>response.json())
- .then(moviedataRandom => {
-        console.log(moviedataRandom)
-
-        // Parse Poster Path
-         let src1 = moviedataRandom.results[radmonNum[0]].poster_path
-         let src2 = moviedataRandom.results[radmonNum[1]].poster_path
-         let src3 = moviedataRandom.results[radmonNum[2]].poster_path
-
-         IdArray= []
-
-         radmonNum.forEach(element => {
-            // console.log(moviedataRandom.results[element].id)
-            IdArray.push(moviedataRandom.results[element].id)
-         });
-
-         console.log(IdArray)
+ / API CALL to FETCH TRENDING  Movie of they day
+ fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=  c1d91b870e49691263e4f7d72633542c")
  
-         //Sets Poster src
-         Poster1.src = `https://image.tmdb.org/t/p/original${src1}`
-         Poster2.src = `https://image.tmdb.org/t/p/original${src2}`
-         Poster3.src = `https://image.tmdb.org/t/p/original${src3}`
+  .then(response =>response.json())
+  .then(moviedataRandom => {
+         console.log(moviedataRandom)
  
-        
-      
-
-//store movie id an array 
-
-        
-     
-
-        //fETCh MOVIE GET MOVIE END POINT VIA ID 
-        IdArray.forEach(element => {
-        fetch (`https://api.themoviedb.org/3/movie/${element}?api_key=c1d91b870e49691263e4f7d72633542c&language=en-US`)
-            
-            .then(res => res.json())
-            .then(movieinfo => {
-                console.log( `
-                Movie Title: ${movieinfo.original_title}
-
-                Movie RunTime: ${movieinfo.runtime}
-
-                Move Overview: ${movieinfo.overview}`)            //dyna display reponse 
-                console.log(movieinfo)
-            })
-            
-        });
-        // for each loop in movie
-      // return fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=c1d91b870e49691263e4f7d72633542c&language=en-US`)
+         // Parse Poster Path
+          let src1 = moviedataRandom.results[radmonNum[0]].poster_path
+          let src2 = moviedataRandom.results[radmonNum[1]].poster_path
+          let src3 = moviedataRandom.results[radmonNum[2]].poster_path
+ 
+          IdArray= []
+ 
+          radmonNum.forEach(element => {
+             // console.log(moviedataRandom.results[element].id)
+             IdArray.push(moviedataRandom.results[element].id)
+          });
+ 
+          console.log(IdArray)
+  
+          //Sets Poster src
+          Poster1.src = `https://image.tmdb.org/t/p/original${src1}`
+          Poster2.src = `https://image.tmdb.org/t/p/original${src2}`
+          Poster3.src = `https://image.tmdb.org/t/p/original${src3}`
+  
          
- })
+       
+ 
+ //store movie id an array 
+ 
+         
+      
+ 
+         //fETCh MOVIE GET MOVIE END POINT VIA ID 
+         IdArray.forEach(element => {
+         fetch (`https://api.themoviedb.org/3/movie/${element}?api_key=  c1d91b870e49691263e4f7d72633542c&language=en-US`)
+             
+             .then(res => res.json())
+             .then(movieinfo => {
+                 console.log( `
+                 Movie Title: ${movieinfo.original_title}
+ 
+                 Movie RunTime: ${movieinfo.runtime}
+ 
+                 Move Overview: ${movieinfo.overview}`)            //dyna display reponse 
+                 console.log(movieinfo)
+             })
+             
+         });
+         // for each loop in movie
+       // return fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=c1d91b870e49691263e4f7d72633542c&language=en-US`)
+          
+  })
+
+
+
+
 
     //     .then(response =>response.json())       //retunred array of movie info 
     //     .then(movieByID => {
